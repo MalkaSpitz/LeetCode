f5cfd12088579525a92d31b6a957f4ab04daa15f
MalkaSpitz <102792235+MalkaSpitz@users.noreply.github.com>
2025-05-20 16:36:47 +0300

    Time: 0 ms (100%), Space: 31.2 MB (16.8%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public bool IsPowerOfTwo(int n) {
+        if(n==1||n==2) return true;
+        if(n%2==1||n<=0) return false;
+        return IsPowerOfTwo(n/2);
+    }
+}
