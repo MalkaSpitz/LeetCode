2cf4e67784dd08a6d57956ec136463d411f0a4d1
MalkaSpitz <102792235+MalkaSpitz@users.noreply.github.com>
2025-05-21 21:52:23 +0300

    Time: 4 ms (28.9%), Space: 43.1 MB (13.11%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cs
+++ 0389-find-the-difference/0389-find-the-difference.cs
@@ -1,12 +1,17 @@
 public class Solution {
     public char FindTheDifference(string s, string t) {
-        s=String.Concat(s.OrderBy(c => c));
-        t=String.Concat(t.OrderBy(c => c));
-        for(int i=0;i<s.Length;i++){
-            if(s[i]!=t[i]){
-                return t[i];
+        s=s+t;
+        Dictionary<char,int> d=new();
+        foreach(char c in s){
+            if(!d.TryAdd(c,1)){
+                d[c]++;
             }
         }
-        return t[t.Length-1];
+        foreach(KeyValuePair<char,int> dd in d){
+            if(dd.Value%2==1){
+                return dd.Key;
+            }
+        }
+        return 'x';
     }
 }
