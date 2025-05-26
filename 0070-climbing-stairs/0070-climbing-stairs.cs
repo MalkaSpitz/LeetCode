b9a00e92ef64f083e073578a71af26af856a3f53
MalkaSpitz <102792235+MalkaSpitz@users.noreply.github.com>
2025-05-26 20:30:34 +0300

    Time: 0 ms (100%), Space: 29.1 MB (39.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cs
+++ 0070-climbing-stairs/0070-climbing-stairs.cs
@@ -1,6 +1,7 @@
 public class Solution {
     public int ClimbStairs(int n) {
-        int[] ways=new int[n+1];
+        if(n<=3) return n;
+        int[] ways=new int[n];
         ways[0]=1;
         ways[1]=2;
         for(int i=2;i<n;i++){
