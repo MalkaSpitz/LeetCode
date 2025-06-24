a9463fc2023e757ca9bdf68a3032c95e8defb3c8
MalkaSpitz <102792235+MalkaSpitz@users.noreply.github.com>
2025-06-24 17:24:14 +0300

    Time: 82 ms (58.54%), Space: 97.5 MB (7.32%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cs
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cs
@@ -1,15 +1,12 @@
 public class Solution {
     public IList<int> FindKDistantIndices(int[] nums, int key, int k) {
-        HashSet<int> inds=new();
+        List<int> res=new();
+
         for(int i=0;i<nums.Length;i++){
             if(nums[i]==key){
-                inds.Add(i);
-            }
-        }
-        List<int> res=new();
-        foreach(int i in inds){
-            for(int j=Math.Max(0,i-k);j<=Math.Min(i+k,nums.Length-1);j++){
-                res.Add(j);
+                for(int j=Math.Max(0,i-k);j<=Math.Min(i+k,nums.Length-1);j++){
+                    res.Add(j);
+                }
             }
         }
         return res.Distinct().OrderBy(o=>o).ToList();
