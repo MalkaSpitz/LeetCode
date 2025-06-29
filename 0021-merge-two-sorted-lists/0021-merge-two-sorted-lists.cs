28fe4d026ff0c76fc748f8517c8c60c33170477f
MalkaSpitz <102792235+MalkaSpitz@users.noreply.github.com>
2025-06-29 12:57:19 +0300

    Time: 0 ms (100%), Space: 43.8 MB (9.28%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cs
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode res=new ListNode();
+        ListNode temp=res;
+        if(list1==null){
+            return list2;
+        }
+        if(list2==null){
+            return list1;
+        }
+        while(list1!=null&&list2!=null){
+            if(list1.val<list2.val){
+                temp.next=new ListNode(list1.val);
+                temp=temp.next;
+                list1=list1.next;
+            }
+            else{
+                temp.next=new ListNode(list2.val);
+                temp=temp.next;
+                list2=list2.next;
+            }
+        }
+        if(list1!=null){
+            temp.next=list1;
+        }
+        if(list2!=null){
+            temp.next=list2;
+        }
+        return res.next;
+    }
+}
