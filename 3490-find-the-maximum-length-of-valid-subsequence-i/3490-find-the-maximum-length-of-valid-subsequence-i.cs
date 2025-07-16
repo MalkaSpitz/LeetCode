62a813b0740887b183a439d57518d7c77fc9bb5a
MalkaSpitz <102792235+MalkaSpitz@users.noreply.github.com>
2025-07-16 10:59:53 +0300

    Time: 4 ms (100%), Space: 76.7 MB (0%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cs
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cs
@@ -2,24 +2,24 @@ public class Solution {
     public int MaximumLength(int[] nums) {
         int countOdd=0;
         int countEven=0;
-        int countZig=0;
-        int check=0;
+        int countSub=0;
+        int carry=0;
         for(int i=0;i<nums.Length;i++){
             if(nums[i]%2==0)
                 countEven++;
             else countOdd++;
 
             if(i==0){
-                check=nums[i]%2;
-                countZig++;
+                carry=nums[i]%2;
+                countSub++;
             }
             else{
-                if(Math.Abs(check-1)==nums[i]%2){
-                    countZig++;
-                    check=Math.Abs(check-1);
+                if(Math.Abs(carry-1)==nums[i]%2){
+                    countSub++;
+                    carry=Math.Abs(carry-1);
                 }
             }
         }
-        return Math.Max(Math.Max(countEven,countOdd),countZig);
+        return Math.Max(Math.Max(countEven,countOdd),countSub);
     }
 }
