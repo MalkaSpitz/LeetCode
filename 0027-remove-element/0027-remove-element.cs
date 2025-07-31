ab7ff3e98616249fbd05047383d984590ce74337
MalkaSpitz <102792235+MalkaSpitz@users.noreply.github.com>
2025-07-31 17:11:50 +0300

    Time: 0 ms (100%), Space: 47 MB (38.82%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int res=0;
+        foreach(int num in nums){
+            if(num!=val){
+                nums[res++]=num;
+            }
+        }
+        return res;
+    }
+}
