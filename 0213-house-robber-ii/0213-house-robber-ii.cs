3446bc56563ba3c4beeb6ba32cea69614d2f8d01
MalkaSpitz <102792235+MalkaSpitz@users.noreply.github.com>
2025-08-08 01:11:04 +0300

    Time: 2 ms (15.08%), Space: 41.3 MB (63.97%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int Rob(int[] nums) {
+        if(nums.Length==1){
+            return nums[0];
+        }
+        return Math.Max(
+            Rob_H(nums.Skip(1).Take(nums.Length-1).ToArray()),
+            Rob_H(nums.Skip(0).Take(nums.Length-1).ToArray()));
+    }
+    private int Rob_H(int[] nums){
+        if(nums.Length==0){
+            return 0;
+        }
+        if(nums.Length==1){
+            return nums[0];
+        }
+        int[] max=new int[nums.Length];
+        max[0]=nums[0];
+        max[1]=Math.Max(nums[0],nums[1]);
+        for(int i=2;i<max.Length;i++){
+            max[i]=Math.Max(max[i-1],nums[i]+max[i-2]);
+        }
+        return Math.Max(max[max.Length-2],max[max.Length-1]);
+    }
+}
